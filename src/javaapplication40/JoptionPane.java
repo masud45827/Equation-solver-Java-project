de9ecdf7b9ce9720931aae5d49ae259c33163d19
masud45827 <masud.cse2019@gmail.com>
2023-11-09 20:13:24 +0600

    Equation Solver java

---
+++ src/javaapplication40/JoptionPane.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication40;
+
+/**
+ *
+ * @author Masud Ali
+ */
+class JoptionPane {
+
+    static int YES_NO_OPTION;
+    
+}
